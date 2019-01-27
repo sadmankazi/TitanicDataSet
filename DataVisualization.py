26378c4700c755495f959b5ebb2d360f2ccdb301
Kazi Sadman <kazisadman2014@u.northwestern.edu>
2019-01-27 00:02:06 -0600

    More figures

--- DataVisualization.py
+++ DataVisualization.py
@@ -43,6 +43,43 @@ plt.title("Gender distribution of Survival")
 plt.xticks(np.arange(2), ('Female', 'Male'), rotation=0)
 plt.ylabel('%')
 
+# survival distribution within ticket class
+fig.add_subplot(3, 1, 2)
+for x in [1, 2, 3]:
+    df.Survived[df.Pclass == x].plot(kind="kde")
+plt.title("Survival Distribution within Classes")
+plt.legend(("1st Class", "2nd Class", "3rd Class"), frameon=False)
+
+# Low class male survived vs deceased
+fig.add_subplot(3, 4, 9)
+df.Survived[(df.Sex == "male") & (df.Pclass == 3)].value_counts(normalize='True').plot(kind='bar', color="lightblue", alpha=alpha_bar_chart)
+plt.title("Low class Male Survival")
+plt.xticks(np.arange(2), ('Dead', 'Alive'), rotation=0)
+plt.ylabel('%')
+
+
+# high class male survived vs deceased
+fig.add_subplot(3, 4, 10)
+df.Survived[(df.Sex == "male") & (df.Pclass == 1)].value_counts(normalize='True').plot(kind='bar', alpha=alpha_bar_chart)
+plt.title("High class Male Survival")
+plt.xticks(np.arange(2), ('Dead', 'Alive'), rotation=0)
+plt.ylabel('%')
+
+# low class female survived vs deceased
+fig.add_subplot(3, 4, 11)
+df.Survived[(df.Sex == "female") & (df.Pclass == 3)].value_counts(normalize='True').plot(kind='bar', color="pink", alpha=alpha_bar_chart)
+plt.title("Low class Female Survival")
+plt.xticks(np.arange(2), ('Alive', 'Dead'), rotation=0)
+plt.ylabel('%')
+
+
+# high class female survived vs  deceased
+fig.add_subplot(3, 4, 12)
+df.Survived[(df.Sex == "female") & (df.Pclass == 1)].value_counts(normalize='True').plot(kind='bar', alpha=alpha_bar_chart)
+plt.title("High class Female Survival")
+plt.xticks(np.arange(2), ('Alive', 'Dead'), rotation=0)
+plt.ylabel('%')
+
 
 plt.tight_layout(pad=0.5, w_pad=1.0, h_pad=1.0)
 plt.show()
